e5444e62c4b5881671c17081768779385b3c48c6
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-08 15:40:53 +0700

    create refund method

--- src/main/java/com/aegis/transaksi/service/TransactionService.java
+++ src/main/java/com/aegis/transaksi/service/TransactionService.java
@@ -3,6 +3,7 @@ package com.aegis.transaksi.service;
 import com.aegis.transaksi.entity.Products;
 import com.aegis.transaksi.entity.Transaction;
 import com.aegis.transaksi.entity.TransactionItem;
+import com.aegis.transaksi.enums.TransactionStatus;
 import com.aegis.transaksi.repository.ProductRepository;
 import com.aegis.transaksi.repository.TransactionItemRepository;
 import com.aegis.transaksi.repository.TransactionRepository;
@@ -73,6 +74,41 @@ public class TransactionService {
 
     public Optional<Transaction> getTransactionById(UUID id){
             return this.transactionRepository.findById(id);
+    }
+
+
+    /*
+        Alur refund:
+    Temukan transaksi id
+    Jika ada, lihat transaksi item id-nya
+    Dengan transaksi_item_id kita bisa lempar id-nya parameter service.
+    Ada? Ya ->
+    Kembalikan jumlah product. Misalnya, tercatat di record: product 1 laku 10,
+    ternyata refund. Di sinilah 10 stok product akan ditambahkan pada yang ada di database.
+
+     */
+
+    public void refund(UUID transactionId){
+        Optional<Transaction> transaction = this.getTransactionById(transactionId);
+
+        if(transaction.isPresent()){
+
+            List<TransactionItem> transactionItems = transaction.get().getTransactionItem();
+
+            for(TransactionItem item : transactionItems){
+                Products product = item.getProduct();
+                int updatedStock = product.getStocks() + item.getQuantity();
+                product.setStocks(updatedStock);
+                productRepository.save(product);
+                transactionItemRepository.delete(item);
+            }
+
+            transaction.get().setStatus(TransactionStatus.REFUNDED);
+            var savedNewTransaction = transaction.get();
+            transactionRepository.save(savedNewTransaction);
+        }else{
+            throw new IllegalArgumentException("Transaksi dengan id " + transactionId + " tidak ditemukan.");
+        }
     }
 
 
