ca28e2bd47aeaccd9546559b82a904ce878eb6a3
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-08 13:35:53 +0700

    edit file

---
+++ src/main/java/com/aegis/transaksi/repository/TransactionRepository.java
@@ -0,0 +1,16 @@
+package com.aegis.transaksi.repository;
+
+import com.aegis.transaksi.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+
+
+}
