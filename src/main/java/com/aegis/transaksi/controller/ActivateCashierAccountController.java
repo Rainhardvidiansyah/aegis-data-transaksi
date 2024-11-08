b7abf9127cfda22fbd93d63c36bae86817f1ca59
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-08 12:36:24 +0700

    activete cashier account controllre

--- src/main/java/com/aegis/transaksi/controller/ActivateCashierAccountController.java
+++ src/main/java/com/aegis/transaksi/controller/ActivateCashierAccountController.java
@@ -1,35 +1,46 @@
 package com.aegis.transaksi.controller;
 
 import com.aegis.transaksi.service.UserService;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/api/v1/aegis/admin")
 public class ActivateCashierAccountController {
 
 
     private final UserService userService;
 
+    private static final Logger logger = LoggerFactory.getLogger(ActivateCashierAccountController.class);
+
     public ActivateCashierAccountController(UserService userService){
         this.userService = userService;
     }
 
+    @GetMapping("/")
+    public String test(){
+        logger.info("This is get hit");
+        return "test";
+    }
 
-    @PutMapping("/activate") //activate?email=rainhard@email.com
-    @PreAuthorize("ROLE_ADMIN")
+    @PutMapping("/activate-cashier") //activate?email=rainhard@email.com
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> activateCashierAccount(@RequestParam String email){
+
+        logger.info("activateCashierAccount just got hit");
 
         var user = userService.getUsers(email);
 
         if(user == null){
             return new ResponseEntity<>("Email tidak ada", HttpStatus.NOT_FOUND);
+        }else if(user.isActive()){
+            return new ResponseEntity<>("Akun telah diaktivasi", HttpStatus.BAD_REQUEST);
         }
 
         this.userService.createActiveuser(user.getEmail());
