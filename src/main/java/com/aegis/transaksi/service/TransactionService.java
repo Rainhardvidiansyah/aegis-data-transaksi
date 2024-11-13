04906d0d3c22613469af23fcc075d81c9b493033
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-14 02:20:14 +0700

    fix bug in transaction service

--- src/main/java/com/aegis/transaksi/service/TransactionService.java
+++ src/main/java/com/aegis/transaksi/service/TransactionService.java
@@ -1,16 +1,21 @@
 package com.aegis.transaksi.service;
 
+import com.aegis.transaksi.dto.TransactionRequestDto;
 import com.aegis.transaksi.entity.Products;
 import com.aegis.transaksi.entity.Transaction;
 import com.aegis.transaksi.entity.TransactionItem;
 import com.aegis.transaksi.enums.TransactionStatus;
 import com.aegis.transaksi.repository.ProductRepository;
 import com.aegis.transaksi.repository.TransactionItemRepository;
-import com.aegis.transaksi.repository.TransactionRepository;
-import jakarta.transaction.Transactional;
+import com.aegis.transaksi.repository.transactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -19,77 +24,162 @@ import java.util.UUID;
 @Service
 public class TransactionService {
 
+        private static final Logger logger = LoggerFactory.getLogger(TransactionService.class);
+
         // menciptakan layer utama untuk laporan.
         // Jadi transaksi ini adalah kumpulan dari data yang ada detail transaksi
         // dalam detail transaksi ada product dan kuantitas product yang dibeli
 
 
-
-        private final TransactionRepository transactionRepository;
+        private final transactionRepository transactionRepository;
         private final ProductRepository productRepository;
         private final TransactionItemRepository transactionItemRepository;
 
-        public TransactionService(TransactionRepository transactionRepository, ProductRepository productRepository, TransactionItemRepository transactionItemRepository) {
+        public TransactionService(com.aegis.transaksi.repository.transactionRepository transactionRepository, ProductRepository productRepository, TransactionItemRepository transactionItemRepository) {
             this.transactionRepository = transactionRepository;
             this.productRepository = productRepository;
             this.transactionItemRepository = transactionItemRepository;
         }
 
+
+
     @Transactional
-    public Transaction saveTransaction(Transaction transaction, List<TransactionItem> transactionItems) {
+    public Transaction createTransaction(List<TransactionRequestDto> transactionRequest) {
+
+        logger.info("This method is called");
+
+        Transaction transaction = new Transaction();
 
 
-        BigDecimal totalAmount = BigDecimal.ZERO;
+        List<TransactionItem> items = new ArrayList<>();
 
-        for (TransactionItem item : transactionItems) {
+        transaction.setStatus(TransactionStatus.COMPLETED);
 
-            item.setTransaction(transaction);
+        transaction.setTransactionDate(LocalDateTime.now());
+        transaction = this.transactionRepository.save(transaction);
 
-            BigDecimal kuantitas = BigDecimal.valueOf(item.getQuantity());
 
-            BigDecimal totalBelanja = item.getPrice().multiply(kuantitas);
-            item.setTotalPrice(totalBelanja);
+        for(TransactionRequestDto requestDto : transactionRequest){
 
-            totalAmount = totalAmount.add(totalBelanja);
+            var product = this.productRepository.findById(requestDto.getProductId());
+            logger.info("product id: {}", requestDto.getProductId());
+            Products products = product.get();
 
-            Products product = item.getProduct();
-
-            int updatedStock = product.getStocks() - item.getQuantity();
-
-            if (updatedStock < 0) {
-                throw new IllegalArgumentException("Stok untuk produk " + product.getProductName() + " tidak cukup.");
+            if(!product.isPresent()){
+                throw new RuntimeException("Product not found");
             }
-            product.setStocks(updatedStock);
-            productRepository.save(product);
-            transactionItemRepository.save(item);
+
+
+            TransactionItem transactionItem = new TransactionItem();
+
+            transactionItem.setProduct(products);
+
+            transactionItem.setPrice(products.getPrice());
+
+            transactionItem.setQuantity(requestDto.getQuantity());
+
+            BigDecimal quantities = BigDecimal.valueOf(requestDto.getQuantity());
+
+
+            BigDecimal totalPrice = products.getPrice().multiply(quantities);
+
+
+            if(totalPrice == null) {
+                throw new RuntimeException("Failed to calculate total price");
+            }
+
+            logger.info("items: {}", items);
+            logger.info("product: {}", products);
+
+            logger.info("TOTAL PRICE: {}", transactionItem.getTotalPrice());
+
+            transactionItem.setTotalPrice(totalPrice);
+
+
+            transactionItem.setTransaction(transaction);
+
+            int newStocks = products.getStocks() - requestDto.getQuantity();
+            products.setStocks(newStocks);
+
+            items.add(transactionItem);
+            List<TransactionItem> listOfTransactionItem = this.transactionItemRepository.saveAll(items);
+
+            listOfTransactionItem.forEach(System.out::println);
+
+            Transaction finalTransaction = transaction;
+            listOfTransactionItem.stream().forEach(transactionItem1 -> transactionItem1.setTransaction(finalTransaction));
+
+
+            logger.info("listOfTransactionItem IS: {}", listOfTransactionItem);
+
+
+            var transactionId = transaction.getTransactionId();
+
+            BigDecimal totalAmount = listOfTransactionItem.stream()
+                    .filter(item -> item.getTransaction().getTransactionId().equals(transactionId))
+                    .map(TransactionItem::getTotalPrice)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+            logger.info("TOTAL AMOUNT: {}", totalAmount);
+
+            if(totalAmount != null){
+                transaction.setTotalAmount(totalAmount);
+            } else {
+                throw new RuntimeException("FAILED TO CALCULATE TOTAL AMOUNT");
+            }
+            logger.info("Transaction Item: {}", transactionItem);
         }
 
-        transaction.setTransactionItem(transactionItems);
-        transaction.setTotalAmount(totalAmount);
-
-        return transactionRepository.save(transaction);
+        logger.info("transaction amount: {}", transaction.getTotalAmount());
+        transaction.addTransactionItem(items);
+        return this.transactionRepository.save(transaction);
     }
 
 
 
-    public Optional<Transaction> getTransactionById(UUID id){
-            return this.transactionRepository.findById(id);
+
+    @Transactional
+    public void addTransaction(UUID productId, int quantity){
+
+        var transaction = new Transaction();
+        transaction.setTransactionDate(LocalDateTime.now());
+
+        List<TransactionItem> items = new ArrayList<>();
+
+        this.transactionRepository.save(transaction);
+
+        var productOptional = this.productRepository.findById(productId);
+
+        var transactionItem = new TransactionItem();
+
+        var product = productOptional.get();
+
+        transactionItem.setProduct(product);
+
+        transactionItem.setPrice(product.getPrice());
+        transactionItem.setQuantity(quantity);
+
+        BigDecimal quantities = BigDecimal.valueOf(quantity);
+        BigDecimal totalPrice = product.getPrice().multiply(quantities);
+
+        transactionItem.setTotalPrice(totalPrice);
+
+        transactionItem.setTransaction(transaction);
+
+        items.add(transactionItem);
+        transaction.addTransactionItem(items);
+        transaction.setTotalAmount(transactionItem.getTotalPrice().multiply(quantities));
+
+        this.transactionRepository.save(transaction);
+
     }
 
 
-    /*
-        Alur refund:
-    Temukan transaksi id
-    Jika ada, lihat transaksi item id-nya
-    Dengan transaksi_item_id kita bisa lempar id-nya parameter service.
-    Ada? Ya ->
-    Kembalikan jumlah product. Misalnya, tercatat di record: product 1 laku 10,
-    ternyata refund. Di sinilah 10 stok product akan ditambahkan pada yang ada di database.
 
-     */
+
 
     public void refund(UUID transactionId){
-        Optional<Transaction> transaction = this.getTransactionById(transactionId);
+        Optional<Transaction> transaction = this.transactionRepository.findById(transactionId);
 
         if(transaction.isPresent()){
 
@@ -112,6 +202,71 @@ public class TransactionService {
     }
 
 
+
 }
+
+
+/*
+sebagai kasir saya akan:
+1. temukan barang -> find product by id
+2. jika ada, saya cari harganya
+3. setelah tahu harganya lalu
+4. saya masukkan kuantitas
+5. dengan adanya harga dan kuantitas, saya akan
+6. jumlahkan kuantitas dan harga dengan mengalikannya.
+7.
+ */
+
+
+            /*
+    UUID transactionItemId, Transaction transaction, Products product, Integer quantity
+    BigDecimal price, BigDecimal totalPrice
+     */
+
+
+/*
+
+ L IS: [TransactionItem(transactionItemI
+d=9be33ac5-65c3-4f03-b692-22cd2945575d, transaction=Transaction(transactionId=419e9acb-7696-4824-8b61-545671b69e44, user=null, transactionDate=2024-11-13T03:23:36.348183
+100, totalAmount=0.000000, status=COMPLETED, transactionItem=null), product=Products(productId=d58e8b59-2b8e-46f7-b9c2-ab0d0f14e8a9, productName=Product A, price=10000.0
+0, stocks=46), quantity=2, price=10000.00, totalPrice=20000.00), TransactionItem(transactionItemId=60afb376-3d70-4b94-9356-7399e4ac3f1d, transaction=Transaction(transact
+ionId=419e9acb-7696-4824-8b61-545671b69e44, user=null, transactionDate=2024-11-13T03:23:36.348183100, totalAmount=0.000000, status=COMPLETED, transactionItem=null), prod
+uct=Products(productId=d58e8b59-2b8e-46f7-b9c2-ab0d0f14e8a9, productName=Product A, price=10000.00, stocks=46), quantity=2, price=10000.00, totalPrice=20000.00), Transac
+tionItem(transactionItemId=325c3f6f-e8da-4fc0-9044-63897b565b0a, transaction=Transaction(transactionId=419e9acb-7696-4824-8b61-545671b69e44, user=null, transactionDate=2
+024-11-13T03:23:36.348183100, totalAmount=0.000000, status=COMPLETED, transactionItem=null), product=Products(productId=c3b7207c-6aaf-45a3-aca6-c64bcaaf52f3, productName
+=Product C, price=15000.00, stocks=34), quantity=3, price=15000.00, totalPrice=45000.00), TransactionItem(transactionItemId=397d6822-d050-4e8d-b4e2-1bca6fac29d4, transac
+tion=Transaction(transactionId=419e9acb-7696-4824-8b61-545671b69e44, user=null, transactionDate=2024-11-13T03:23:36.348183100, totalAmount=0.000000, status=COMPLETED, tr
+ansactionItem=null), product=Products(productId=c3b7207c-6aaf-45a3-aca6-c64bcaaf52f3, productName=Product C, price=15000.00, stocks=34), quantity=3, price=15000.00, totalPrice=45000.00)]
+
+ */
+
+/*
+        Alur refund:
+    Temukan transaksi id
+    Jika ada, lihat transaksi item id-nya
+    Dengan transaksi_item_id kita bisa lempar id-nya parameter service.
+    Ada? Ya ->
+    Kembalikan jumlah product. Misalnya, tercatat di record: product 1 laku 10,
+    ternyata refund. Di sinilah 10 stok product akan ditambahkan pada yang ada di database.
+
+     */
+
+
+
+    /*
+        Alur refund:
+    Temukan transaksi id
+    Jika ada, lihat transaksi item id-nya
+    Dengan transaksi_item_id kita bisa lempar id-nya parameter service.
+    Ada? Ya ->
+    Kembalikan jumlah product. Misalnya, tercatat di record: product 1 laku 10,
+    ternyata refund. Di sinilah 10 stok product akan ditambahkan pada yang ada di database.
+
+     */
+
+
+
+
+
 
 
