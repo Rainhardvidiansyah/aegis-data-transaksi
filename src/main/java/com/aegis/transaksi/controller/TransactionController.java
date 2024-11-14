3638a3917ce179f1ac8201992f3a3be3b9853c06
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-15 01:23:03 +0700

    add validation and response in transaction controller

--- src/main/java/com/aegis/transaksi/controller/TransactionController.java
+++ src/main/java/com/aegis/transaksi/controller/TransactionController.java
@@ -2,14 +2,19 @@ package com.aegis.transaksi.controller;
 
 
 import com.aegis.transaksi.dto.TransactionRequest;
+import com.aegis.transaksi.dto.TransactionRequestDto;
+import com.aegis.transaksi.dto.responses.TransactionResponseDto;
 import com.aegis.transaksi.entity.Transaction;
-import com.aegis.transaksi.entity.TransactionItem;
+
 import com.aegis.transaksi.service.TransactionService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -27,9 +32,24 @@ public class TransactionController {
 
     @PostMapping("/save")
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CASHIER')")
-    public ResponseEntity<Transaction> saveTransaction(@RequestBody TransactionRequest request) {
+    public ResponseEntity<?> saveTransaction(@RequestBody @Valid TransactionRequestDto request, BindingResult bindingResult) {
 
-        return null;
+        if(bindingResult.hasErrors()){
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
+        }
+
+        var transactionRequest = new TransactionRequestDto(request.getProductId(), request.getQuantity());
+
+        List<TransactionRequestDto> transactionRequestList = new ArrayList<>();
+
+        transactionRequestList.add(transactionRequest);
+
+        var transaction = transactionService.createTransaction(transactionRequestList);
+
+        var response = TransactionResponseDto.response(transaction);
+
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
 
     }
 
