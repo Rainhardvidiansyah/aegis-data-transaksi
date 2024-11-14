a9f72de55b9a6f9794069ff85163fca934047050
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-15 01:24:14 +0700

    add hibernate validation in TransactionRequestDto

--- src/main/java/com/aegis/transaksi/dto/TransactionRequestDto.java
+++ src/main/java/com/aegis/transaksi/dto/TransactionRequestDto.java
@@ -1,6 +1,8 @@
 package com.aegis.transaksi.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,8 +16,11 @@ import java.util.UUID;
 public class TransactionRequestDto {
 
 
+    @NotNull(message = "Product Id tidak boleh null")
     private UUID productId;
 
+    @Size(min = 1, message = "Kuantitas tidak boleh kurang dari 1")
+    @NotNull(message = "Null tidak boleh digunakan di sini")
     private int quantity;
 
     public TransactionRequestDto(UUID productId, int quantity) {
