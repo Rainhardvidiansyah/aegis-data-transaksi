93afb70b8d3c9c6b2b2419d66751ee326d2d9631
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-23 01:57:28 +0700

    create new query

--- src/main/java/com/aegis/transaksi/repository/TransactionItemRepository.java
+++ src/main/java/com/aegis/transaksi/repository/TransactionItemRepository.java
@@ -2,10 +2,29 @@ package com.aegis.transaksi.repository;
 
 import com.aegis.transaksi.entity.TransactionItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TransactionItemRepository extends JpaRepository<TransactionItem, UUID> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM TransactionItem WHERE" +
+            "transaction_item_id = :transaction_item_id" +
+            "AND transaction_id = :transaction_id")
+    Optional<TransactionItem> findTransactionItemByTransactionId(@Param("transaction_item_id") UUID transactionItemId, @Param("transaction_id") UUID transaction_id);
+
+    /*
+    @Modifying
+@Query(nativeQuery = true, value = "UPDATE TransactionItem SET quantity = quantity + :quantity, " +
+        "total_price = (price * (quantity + :quantity)) " +
+        "WHERE transaction_item_id = :transaction_item_id AND transaction_id = :transaction_id")
+int updateTransactionItemQuantity(@Param("transaction_item_id") UUID transactionItemId,
+                                   @Param("transaction_id") UUID transactionId,
+                                   @Param("quantity") int quantity);
+
+     */
 }
