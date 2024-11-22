6babb53a0cd876ebe1307213b4273d62b4f43314
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-11-23 02:06:49 +0700

    Add some methods in transaction service class.

--- src/main/java/com/aegis/transaksi/service/TransactionService.java
+++ src/main/java/com/aegis/transaksi/service/TransactionService.java
@@ -9,6 +9,7 @@ import com.aegis.transaksi.exceptions.ProductNotFoundException;
 import com.aegis.transaksi.repository.ProductRepository;
 import com.aegis.transaksi.repository.TransactionItemRepository;
 import com.aegis.transaksi.repository.TransactionRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -25,23 +26,23 @@ import java.util.UUID;
 @Service
 public class TransactionService {
 
-        private static final Logger logger = LoggerFactory.getLogger(TransactionService.class);
+    private static final Logger logger = LoggerFactory.getLogger(TransactionService.class);
 
-        // menciptakan layer utama untuk laporan.
-        // Jadi transaksi ini adalah kumpulan dari data yang ada detail transaksi
-        // dalam detail transaksi ada product dan kuantitas product yang dibeli
+    // menciptakan layer utama untuk laporan.
+    // Jadi transaksi ini adalah kumpulan dari data yang ada detail transaksi
+    // dalam detail transaksi ada product dan kuantitas product yang dibeli
 
 
-        private final TransactionRepository transactionRepository;
-        private final ProductRepository productRepository;
-        private final TransactionItemRepository transactionItemRepository;
+    private final TransactionRepository transactionRepository;
+    private final ProductRepository productRepository;
+    private final TransactionItemRepository transactionItemRepository;
 
-        public TransactionService(TransactionRepository transactionRepository, ProductRepository productRepository, TransactionItemRepository transactionItemRepository) {
-            this.transactionRepository = transactionRepository;
-            this.productRepository = productRepository;
-            this.transactionItemRepository = transactionItemRepository;
-        }
-
+    public TransactionService(TransactionRepository transactionRepository, ProductRepository productRepository,
+                              TransactionItemRepository transactionItemRepository) {
+        this.transactionRepository = transactionRepository;
+        this.productRepository = productRepository;
+        this.transactionItemRepository = transactionItemRepository;
+    }
 
 
     @Transactional
@@ -60,7 +61,7 @@ public class TransactionService {
         transaction = this.transactionRepository.save(transaction);
 
 
-        for(TransactionRequestDto requestDto: transactionRequest){
+        for (TransactionRequestDto requestDto : transactionRequest) {
 
             UUID productId = UUID.fromString(requestDto.getProductId());
 
@@ -68,7 +69,7 @@ public class TransactionService {
 
             Products products = product.get();
 
-            if(!product.isPresent()){
+            if (!product.isPresent()) {
                 throw new ProductNotFoundException("Product not found");
             }
 
@@ -84,7 +85,7 @@ public class TransactionService {
             BigDecimal totalPrice = products.getPrice().multiply(quantities);
 
 
-            if(totalPrice == null) {
+            if (totalPrice == null) {
                 throw new RuntimeException("Failed to calculate total price");
             }
 
@@ -122,7 +123,7 @@ public class TransactionService {
 
             logger.info("TOTAL AMOUNT: {}", totalAmount);
 
-            if(totalAmount != null){
+            if (totalAmount != null) {
                 transaction.setTotalAmount(totalAmount);
             } else {
                 throw new RuntimeException("FAILED TO CALCULATE TOTAL AMOUNT");
@@ -136,10 +137,8 @@ public class TransactionService {
     }
 
 
-
-
     @Transactional
-    public void addTransaction(UUID productId, int quantity){
+    public void addTransaction(UUID productId, int quantity) {
 
         var transaction = new Transaction();
         transaction.setTransactionDate(LocalDateTime.now());
@@ -175,32 +174,207 @@ public class TransactionService {
     }
 
 
+    //find transaction
+    //if found, then get transaction_item
+    //if transaction_item is found, then
+    //get product from transaction_item and
+    //add with newQuantity
+    @Transactional
+    public Transaction addProductAndUpdateTotalPrice(String transactionId, String transactionItemsid, int quantity) {
+        logger.info("addProductAndUpdateTotalPrice");
 
 
+        UUID transactionUuid = UUID.fromString(transactionId);
+        var transactionOptional = transactionRepository.findById(transactionUuid);
 
-    public void refund(UUID transactionId){
-        Optional<Transaction> transaction = this.transactionRepository.findById(transactionId);
+        Transaction transaction = transactionOptional
+                .orElseThrow(() -> new RuntimeException("Transaction not found TTTTTTTTTT"));
 
-        if(transaction.isPresent()){
 
-            List<TransactionItem> transactionItems = transaction.get().getTransactionItem();
-
-            for(TransactionItem item : transactionItems){
-                Products product = item.getProduct();
-                int updatedStock = product.getStocks() + item.getQuantity();
-                product.setStocks(updatedStock);
-                productRepository.save(product);
-                transactionItemRepository.delete(item);
-            }
-
-            transaction.get().setStatus(TransactionStatus.REFUNDED);
-            var savedNewTransaction = transaction.get();
-            transactionRepository.save(savedNewTransaction);
-        }else{
-            throw new IllegalArgumentException("Transaksi dengan id " + transactionId + " tidak ditemukan.");
+        if (transaction == null) {
+            throw new RuntimeException("Transaction not found");
         }
+
+        UUID transactionItemUuid = UUID.fromString(transactionItemsid);
+
+        var transactionItem = transaction.getTransactionItem().stream()
+                .filter(item -> item.getTransactionItemId().equals(transactionItemUuid))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Transaction Item not found"));
+
+        //validate that specific transaction_id belongs to some or many
+        //transaction item.
+        //Without validating it, the program will be chaos
+        if (!transaction.getTransactionId().equals(transactionItem.getTransaction().getTransactionId())) {
+            throw new IllegalStateException("Id is not same");
+        }
+
+
+        //start to update quantity of product in transaction_item
+
+        int newQuantityInTransactionItems = transactionItem.getQuantity() + quantity;
+
+        //get product price
+        //product price here should be multiplied by newQuantityInTransactionItems
+
+        BigDecimal productPrice = transactionItem.getProduct().getPrice();
+        BigDecimal newTotalPrice = productPrice.multiply(BigDecimal.valueOf(newQuantityInTransactionItems));
+
+        transactionItem.setQuantity(newQuantityInTransactionItems);
+        reduceStockInProduct(transactionItem.getProduct().getProductId(), quantity);
+
+        transactionItem.setTotalPrice(newTotalPrice);
+
+
+        //insert Transaction Item to list of transaction item
+        List<TransactionItem> items = new ArrayList<>();
+        items.add(transactionItem);
+
+
+        BigDecimal totalAmount = items.stream()
+                .filter(item -> item.getTransaction().getTransactionId().equals(transaction.getTransactionId()))
+                .map(TransactionItem::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        transaction.setTotalAmount(totalAmount);
+
+        transactionItemRepository.saveAll(items);
+
+        transaction.getTransactionItem().clear();
+        transaction.getTransactionItem().addAll(items);
+
+
+        //transaction.addTransactionItem(items);
+        transaction.addAllItem(items);
+
+
+        return transactionRepository.save(transaction);
     }
 
+    // error: A collection with cascade="all-delete-orphan" was no longer referenced by the owning entity instance: com.aegis.transaksi.entity.Transaction.transactionItem
+    /*
+    Begini langkahnya:
+    cari transaksinya dulu.
+    ada transaksinya? oh, ada.
+    ambil transaction itemnya.
+    ada transaction item? yup, ada.
+
+    Jika ada jangan lupa untuk disamakan bahwa: satu transaction id
+    bisa terkait dengan banyak transaction_item_id. Harus divalidasi dulu!
+
+    Pada transaction_item, kita punya beberapa variable yang bisa digunakan:
+    "harga satuan" dikali kuantitas baru (di param).
+    "Kuantitas" yang ada ditambah kuantitas baru (di param).
+     */
+
+
+    @Transactional
+    private void reduceStockInProduct(UUID productId, int quantity) {
+
+        var productOptional = this.productRepository.findById(productId);
+
+        if (!productOptional.isPresent()) {
+            throw new ProductNotFoundException("Product not found");
+        }
+
+        Products products = productOptional.get();
+
+        int newStocks = products.getStocks() - quantity;
+
+        if (newStocks < 0) {
+            throw new IllegalStateException("Out of Stock");
+        }
+
+        products.setStocks(newStocks);
+
+        productRepository.save(products);
+    }
+
+
+    @Transactional
+    public Transaction subtractTransactionAndUpdateTotalAMount(String transactionId, String transactionItemId, int quantity) {
+
+        UUID transactionUuid = UUID.fromString(transactionId);
+        UUID transactionItemUuid = UUID.fromString(transactionItemId);
+
+        List<TransactionItem> items = new ArrayList<>();
+
+        Optional<Transaction> transactionOptional = transactionRepository.findById(transactionUuid);
+        var transaction = transactionOptional.orElseThrow(() -> new RuntimeException("Could not find transaction"));
+
+        var transactionItem = transaction.getTransactionItem().stream()
+                .filter(item -> item.getTransactionItemId().equals(transactionItemUuid))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Could not find transaction item"));
+
+        //validate that specific transaction_id belongs to some or many
+        //transaction item.
+        //Without validating it, the program will be chaos
+        if (!transaction.getTransactionId().equals(transactionItem.getTransaction().getTransactionId())) {
+            throw new IllegalStateException("Id is not same!!");
+        }
+
+        BigDecimal productPrice = transactionItem.getProduct().getPrice();
+
+        int newQuantity = transactionItem.getQuantity() - quantity;
+
+        BigDecimal totalPrice = productPrice.multiply(BigDecimal.valueOf(newQuantity));
+
+        transactionItem.setTotalPrice(totalPrice);
+
+
+        items.add(transactionItem);
+        //recount the total price by counting for each total price that belongs to transaction_item
+        //then this total amount variable will be set at transaction total amount
+        BigDecimal totalAmount = items.stream()
+                .filter(item -> item.getTransaction().getTransactionId().equals(transaction.getTransactionId()))
+                .map(TransactionItem::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::subtract);
+
+
+        this.subtractProductFromTTransaction(transactionItem.getProduct().getProductId(), newQuantity);
+
+        transactionItemRepository.saveAll(items);
+
+        transaction.getTransactionItem().clear();
+        transaction.getTransactionItem().addAll(items);
+        transaction.setTransactionItem(items);
+        transaction.setTotalAmount(totalAmount);
+
+        return transactionRepository.save(transaction);
+    }
+
+
+    @Transactional
+    private void subtractProductFromTTransaction(UUID productId, int quantity) {
+
+        var product = productRepository.findById(productId);
+
+        Products products = product.orElseThrow(() -> new RuntimeException("Product not found"));
+
+        int newStock = products.getStocks() + quantity;
+
+        products.setStocks(newStock);
+
+        productRepository.save(products);
+    }
+
+
+    //GET TRANSACTION BY ID
+    public Transaction getTransactionById(String transactionId) {
+
+        UUID transactionUuid = UUID.fromString(transactionId);
+
+        var transactionOptional = transactionRepository.findById(transactionUuid);
+
+        return transactionOptional.orElseThrow(() -> new RuntimeException("Could not find transaction"));
+    }
+
+
+    //GET ALL TRANSACTIONS
+    public Iterable<Transaction> getAllTransactions() {
+        return this.transactionRepository.findAll();
+    }
 
 
 }
@@ -218,37 +392,7 @@ sebagai kasir saya akan:
  */
 
 
-            /*
-    UUID transactionItemId, Transaction transaction, Products product, Integer quantity
-    BigDecimal price, BigDecimal totalPrice
-     */
 
-
-
-
-/*
-        Alur refund:
-    Temukan transaksi id
-    Jika ada, lihat transaksi item id-nya
-    Dengan transaksi_item_id kita bisa lempar id-nya parameter service.
-    Ada? Ya ->
-    Kembalikan jumlah product. Misalnya, tercatat di record: product 1 laku 10,
-    ternyata refund. Di sinilah 10 stok product akan ditambahkan pada yang ada di database.
-
-     */
-
-
-
-    /*
-        Alur refund:
-    Temukan transaksi id
-    Jika ada, lihat transaksi item id-nya
-    Dengan transaksi_item_id kita bisa lempar id-nya parameter service.
-    Ada? Ya ->
-    Kembalikan jumlah product. Misalnya, tercatat di record: product 1 laku 10,
-    ternyata refund. Di sinilah 10 stok product akan ditambahkan pada yang ada di database.
-
-     */
 
 
 
